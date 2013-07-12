e164df178eb72276a08ee643e1548cad4cbaffea
James McCarthy <jmccarth@uwaterloo.ca>
2013-07-12 11:25:43 -0400

    Show list of researchers on a dialog and list a selected researcher's space

--- js/helper.js
+++ js/helper.js
@@ -15,6 +15,23 @@ function findRoom(roomNum) {
 	});
 }
 
+function findRoomBySpaceID(spaceid) {
+	var query = new esri.tasks.Query();
+	query.where = "SPACEID = '" + spaceid + "'";
+
+	query.outFields = ["*"];
+	var floorQueryTask = new esri.tasks.QueryTask(roomsLayerURL);
+	floorQueryTask.execute(query, function(results) {
+		floorkey = results.features[0].attributes["FLOORKEY"];
+		changeFloor(floorkey.substr(floorkey.length - 1, 1));
+		roomsLayer.selectFeatures(query, esri.layers.FeatureLayer.SELECTION_NEW, function(features) {
+			//zoom to the selected feature
+			var roomExtent = features[0].geometry.getExtent().expand(5.0);
+			map.setExtent(roomExtent);
+		});
+	});
+}
+
 function clear() {
 	roomsLayer.clearSelection();
 	map.infoWindow.hide();
--- js/research.js
+++ js/research.js
@@ -15,3 +15,39 @@ function populateResearcherView(spaceid){
 		}
 	});
 }
+
+function showResearchers(){
+	dojo.byId("showSpacesBtn").disabled = true; //user selection will enable
+	dojo.byId("researcherSpaces").innerHTML = "" //TODO: probably a cleaner way to do this
+	dojo.byId("researcherList").innerHTML = "" //TODO: probably a cleaner way to do this
+	var rQuery = new esri.tasks.Query();
+	rQuery.where = "1=1";
+	
+	var rQueryTask = new esri.tasks.QueryTask(researchersURL);
+	rQueryTask.execute(rQuery,function(results){
+		for (f in results.features){
+			rName = results.features[f].attributes.USERNAME;
+			dojo.byId("researcherList").options.add(new Option(rName, rName));
+		}
+	});
+	show('researcherDialog');
+}
+
+function listResearcherSpaces(userName){
+	dojo.byId("researcherSpaces").innerHTML = "" //TODO: probably a cleaner way to do this
+	var rQuery = new esri.tasks.Query();
+	rQuery.where = "RESEARCHER = '" + userName + "'";
+	rQuery.outFields = ["*"];
+	
+	var rQueryTask = new esri.tasks.QueryTask(researcherSpaceURL);
+	rQueryTask.execute(rQuery,function(results){
+		
+			dojo.byId("showSpacesBtn").disabled = true; //user selection will enable
+			for (f in results.features){
+				sID = results.features[f].attributes.SPACE_ID;
+				dojo.byId("researcherSpaces").options.add(new Option(sID, sID));
+			}
+		
+	});
+
+}
